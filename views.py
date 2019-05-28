5f8794c5523eb02604a97d7cba0880d38a166212
Adilene Pulgarin <ap739s@att.com>
2019-05-27 19:57:27 -0700

    Working on app css

--- views.py
+++ views.py
@@ -419,4 +419,4 @@ def getUserID(email):
 if __name__ == '__main__':
     app.secret_key = 'DEV_SECRET_KEY'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 5050)
